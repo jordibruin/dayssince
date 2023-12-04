cb78f317034043f2bb6e513fb3062a1dcac7a028
Jordi Bruin <jordi@goodsnooze.com>
2023-12-04 17:15:46 +0100

    added the old item struct for migration purposes

--- DaysSince/Model/DSItem.swift
+++ DaysSince/Model/DSItem.swift
@@ -55,3 +55,45 @@ struct DSItem: Identifiable, Codable {
         return DSItem(id: UUID(), name: "Placeholder", category: category, dateLastDone: Date.now, remindersEnabled: false)
     }
 }
+
+struct oldDSItem: Identifiable, Codable {
+    let id: UUID
+
+    /// The name of the item.
+    var name: String
+
+    /// Category of the item.
+    var category = CategoryDSIte.work
+
+    /// Day last done.
+    var dateLastDone: Date
+
+    // Whether the item sends reminders.
+    var remindersEnabled: Bool
+
+    // What type of reminder (daily, weekly, monthly)
+    var reminder: DSItemReminders = .daily
+
+    // We're no longer using this.
+    // Date when item was completed (it was over).
+    var dateCompleted: Date = .now
+
+    /// The emoji of the item.
+    var emoji: String {
+        return category.sfSymbolName
+    }
+
+    /// The ID of the repeating notification reminder.
+    var reminderNotificationID: String = UUID().uuidString
+
+    /// String for number of days since you did it.
+    var daysAgo: Int {
+        let daysSince = Calendar.current.numberOfDaysBetween(dateLastDone, and: Date.now)
+        return abs(daysSince)
+    }
+
+    var completedDaysAgo: Int {
+        let daysSince = Calendar.current.numberOfDaysBetween(dateCompleted, and: Date.now)
+        return abs(daysSince)
+    }
+}
