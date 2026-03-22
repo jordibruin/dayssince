43c8863f1af52b71d55ec9b120279995221605d9
vickipetrova <victoria.petrova@uni.minerva.edu>
2026-03-22 16:25:24 +0000

    ui: main screen now has bottom toolbar with add new event and category buttons

--- DaysSince/DaysSince/MainScreen/MainScreen.swift
+++ DaysSince/DaysSince/MainScreen/MainScreen.swift
@@ -20,6 +20,7 @@ struct MainScreen: View {
     @EnvironmentObject var dataSyncManager: DataSyncManager
 
     @State var showAddItemSheet = false
+    @State var showAddCategorySheet = false
     @State var showSettings = false
     @State var editItemSheet = false
     @State var tappedItem: DSItem = .placeholderItem()
@@ -82,16 +83,13 @@ struct MainScreen: View {
                     )
                 }
 
-                VStack {
-                    Spacer()
-                    addNewEventButton
-                }
             }
             .navigationTitle("Events")
             .navigationBarTitleDisplayMode(.inline)
-            .toolbar(content: {
+            .toolbar {
                 toolbarItems
-            })
+                bottomToolbarItems
+            }
         }
         .navigationViewStyle(.stack)
         .sheet(isPresented: $showAddItemSheet) {
@@ -105,6 +103,9 @@ struct MainScreen: View {
                 .presentationDetents([.medium])
                 .presentationCornerRadius(32)
                 .onDisappear { showThemeSheet = false }
+        }
+        .sheet(isPresented: $showAddCategorySheet) {
+            AddCategorySheet()
         }
         .sheet(isPresented: $showSupportScreen) {
             NavigationView {
@@ -158,31 +159,26 @@ struct MainScreen: View {
         }
     }
 
-    var addNewEventButton: some View {
-        Button {
-            showAddItemSheet = true
-        } label: {
-            HStack {
-                Image(systemName: "plus")
-                    .font(.system(.title2, design: .rounded))
-                    .foregroundColor(.white)
-                Text("Add New Event")
-                    .font(.system(.title2, design: .rounded))
-                    .bold()
-                    .foregroundColor(.white)
+    var bottomToolbarItems: some ToolbarContent {
+        ToolbarItemGroup(placement: .bottomBar) {
+            Spacer()
+
+            Button {
+                showAddCategorySheet = true
+            } label: {
+                Label("New Category", systemImage: "folder.badge.plus")
             }
-            .padding()
-            .background(LinearGradient(
-                gradient: .init(colors: [colorScheme == .dark ? mainColor.opacity(0.85).darker(by: 0.1) : mainColor.opacity(0.85), colorScheme == .dark ? mainColor.darker(by: 0.2) : mainColor]),
-                startPoint: .init(x: 0.0, y: 0.5),
-                endPoint: .init(x: 0, y: 1)
-            ))
-            .background(colorScheme == .dark ? Color.black : Color.white)
-            .clipShape(Capsule())
-            .shadow(color: mainColor, radius: 10, x: 0, y: 5)
+
+//            Spacer()
+
+            Button {
+                showAddItemSheet = true
+            } label: {
+                Label("New Event", systemImage: "plus")
+            }
+
+            Spacer()
         }
-        .buttonStyle(PlainButtonStyle())
-        .padding(.bottom, 16)
     }
 }
 
